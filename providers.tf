0847e10f6f8be22cf88391cf85f9b0f784ff4f7f
Grant Birkinbine <grantbirki@github.com>
2022-03-28 16:13:01 -0700

    init

---
+++ providers.tf
@@ -0,0 +1,21 @@
+terraform {
+    backend "remote" {
+    organization = "the-hideout"
+
+    workspaces {
+      name = "cloudflare"
+    }
+  }
+
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "3.11.0"
+    }
+  }
+  required_version = "1.1.7"
+}
+
+provider "cloudflare" {
+  api_token = var.CLOUDFLARE_TOKEN
+}
