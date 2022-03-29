f291d56ced9368c30003e5d2c399c5ecb990631c
Grant Birkinbine <grantbirki@github.com>
2022-03-28 17:30:39 -0700

    add more dns records

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -6,3 +6,30 @@ resource "cloudflare_record" "api" {
     value       = "api.hideout-api.workers.dev"
     zone_id     = var.CLOUDFLARE_ZONE_ID
 }
+
+resource "cloudflare_record" "assets" {
+    name        = "assets"
+    proxied     = true
+    ttl         = 1
+    type        = "CNAME"
+    value       = "assets.thehideout.io.s3-website-us-east-1.amazonaws.com"
+    zone_id     = var.CLOUDFLARE_ZONE_ID
+}
+
+resource "cloudflare_record" "manager" {
+    name        = "manager"
+    proxied     = false
+    ttl         = 60
+    type        = "A"
+    value       = "20.115.107.210"
+    zone_id     = var.CLOUDFLARE_ZONE_ID
+}
+
+resource "cloudflare_record" "status" {
+    name        = "status"
+    proxied     = false
+    ttl         = 60
+    type        = "A"
+    value       = "20.228.137.173"
+    zone_id     = var.CLOUDFLARE_ZONE_ID
+}
