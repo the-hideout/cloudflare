efbe78c9446c657851c5afd568edfcc6a64f23e2
Grant Birkinbine <grantbirki@github.com>
2022-03-30 20:55:09 -0600

    add ratelimit

---
+++ terraform/security.tf
@@ -0,0 +1,37 @@
+resource "cloudflare_rate_limit" "tarkov_dev" {
+    bypass_url_patterns = []
+    description         = "API"
+    disabled            = false
+    period              = 10
+    threshold           = 100
+    zone_id             = var.CLOUDFLARE_ZONE_ID
+
+    action {
+        mode    = "ban"
+        timeout = 3600
+    }
+
+    match {
+        request {
+            methods     = [
+                "_ALL_",
+            ]
+            schemes     = [
+                "_ALL_",
+            ]
+            url_pattern = "api.tarkov.dev/*"
+        }
+
+        response {
+            headers        = [
+                {
+                    "name"  = "Cf-Cache-Status"
+                    "op"    = "ne"
+                    "value" = "HIT"
+                },
+            ]
+            origin_traffic = true
+            statuses       = []
+        }
+    }
+}
