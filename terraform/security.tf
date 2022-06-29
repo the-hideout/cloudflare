5038f119608a74c35768a9de7712562ad6e626e5
Thaddeus Bond <thaddeus@thaddeusbond.com>
2022-06-29 12:37:03 +0000

    Add terraform rules for blocking Amazon got attacker

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,7 +1,20 @@
+resource "cloudflare_filter" "block_amazon_aes_got_attacker" {
+  zone_id = var.CLOUDFLARE_ZONE_ID
+  description = "Block Amazon AES POST attacker using 'got'"
+  expression = "(http.user_agent contains "got (https://github.com/sindresorhus/got)" and ip.geoip.asnum eq 14618 and http.request.uri.path eq "/graphql")"
+}
+
+resource "cloudflare_firewall_rule" "block_amazon_aes_got_attacker" {
+  zone_id = var.CLOUDFLARE_ZONE_ID
+  description = "Block Amazon AES POST attacker using 'got'"
+  filter_id = cloudflare_filter.block_amazon_aes_got_attacker.id
+  action = "block"
+}
+
 resource "cloudflare_rate_limit" "tarkov_dev" {
   bypass_url_patterns = []
   description         = "API"
-  disabled            = false
+  disabled            = true
   period              = 10
   threshold           = 100
   zone_id             = var.CLOUDFLARE_ZONE_ID
