e0e0d27b89f041ece2b8880801001651f9ef0827
Grant Birkinbine <grantbirki@github.com>
2022-08-16 11:56:25 -0600

    init security headers

---
+++ terraform/transform_rules.tf
@@ -0,0 +1,32 @@
+# Change origin for an API route
+resource "cloudflare_ruleset" "security_response_headers" {
+  zone_id     = var.CLOUDFLARE_ZONE_ID
+  name        = "Security Response Headers"
+  description = "Sets security response headers"
+  kind        = "zone"
+  phase       = "http_response_headers_transform"
+
+  rules {
+    action = "rewrite"
+    action_parameters {
+      headers {
+        name      = "X-Frame-Options"
+        value     = "DENY"
+        operation = "set"
+      }
+      headers {
+        name      = "X-XSS-Protection"
+        value     = "1; mode=block"
+        operation = "set"
+      }
+      headers {
+        name      = "Content-Security-Policy"
+        value     = "img-src 'self' https://assets.tarkov.dev data: https://images.weserv.nl; style-src 'self' https://tarkov.dev https://discord.com;"
+        operation = "set"
+      }
+    }
+    expression  = "(not http.host contains \"api\")"
+    enabled     = true
+    description = "Sets security response headers"
+  }
+}
