a9cd43223d56ee6bf10bc71666e55ec9f36541e6
Grant Birkinbine <grantbirki@github.com>
2022-08-17 09:43:52 -0600

    redirect www

--- terraform/page_rules.tf
+++ terraform/page_rules.tf
@@ -1,17 +1,19 @@
 resource "cloudflare_page_rule" "www_redirect" {
   priority = 1
   status   = "active"
-  target   = "cache.tarkov.dev/*"
+  target   = "www.tarkov.dev"
   zone_id  = var.CLOUDFLARE_ZONE_ID
 
   actions {
-    cache_level = "cache_everything"
-    ssl         = "strict"
+    forwarding_url {
+      url = "https://tarkov.dev"
+      status_code = 301
+    }
   }
 }
 
 resource "cloudflare_page_rule" "cache_service" {
-  priority = 1
+  priority = 2
   status   = "active"
   target   = "cache.tarkov.dev/*"
   zone_id  = var.CLOUDFLARE_ZONE_ID
@@ -23,7 +25,7 @@ resource "cloudflare_page_rule" "cache_service" {
 }
 
 resource "cloudflare_page_rule" "status_services" {
-  priority = 2
+  priority = 3
   status   = "active"
   target   = "status.tarkov.dev/*"
   zone_id  = var.CLOUDFLARE_ZONE_ID
@@ -35,7 +37,7 @@ resource "cloudflare_page_rule" "status_services" {
 }
 
 resource "cloudflare_page_rule" "status_custom_cache" {
-  priority = 3
+  priority = 4
   status   = "active"
   target   = "status.tarkov.dev/api/status-page/heartbeat/api"
   zone_id  = var.CLOUDFLARE_ZONE_ID
@@ -47,7 +49,7 @@ resource "cloudflare_page_rule" "status_custom_cache" {
 }
 
 resource "cloudflare_page_rule" "manager_service" {
-  priority = 4
+  priority = 5
   status   = "active"
   target   = "manager.tarkov.dev/*"
   zone_id  = var.CLOUDFLARE_ZONE_ID
@@ -58,7 +60,7 @@ resource "cloudflare_page_rule" "manager_service" {
 }
 
 resource "cloudflare_page_rule" "data_json" {
-  priority = 5
+  priority = 6
   status   = "active"
   target   = "tarkov.dev/data/*.json"
   zone_id  = var.CLOUDFLARE_ZONE_ID
@@ -71,7 +73,7 @@ resource "cloudflare_page_rule" "data_json" {
 }
 
 resource "cloudflare_page_rule" "fonts" {
-  priority = 6
+  priority = 7
   status   = "active"
   target   = "tarkov.dev/fonts/*"
   zone_id  = var.CLOUDFLARE_ZONE_ID
@@ -84,7 +86,7 @@ resource "cloudflare_page_rule" "fonts" {
 }
 
 resource "cloudflare_page_rule" "images" {
-  priority = 7
+  priority = 8
   status   = "active"
   target   = "tarkov.dev/images/*"
   zone_id  = var.CLOUDFLARE_ZONE_ID
